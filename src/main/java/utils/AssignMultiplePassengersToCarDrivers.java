3bddbe8d6cf0aec16bc9809da9c055554d9a54bd
kaghog <grace.kagho@ivt.baug.ethz.ch>
2023-03-29 09:24:37 +0200

    include option to update matched passengers with driver's plans

--- src/main/java/utils/AssignMultiplePassengersToCarDrivers.java
+++ src/main/java/utils/AssignMultiplePassengersToCarDrivers.java
@@ -2,10 +2,7 @@ package utils;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.population.Leg;
-import org.matsim.api.core.v01.population.Person;
-import org.matsim.api.core.v01.population.Population;
-import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -29,7 +26,7 @@ public class AssignMultiplePassengersToCarDrivers {
     public static void main(String[] args) throws IOException, CommandLine.ConfigurationException {
         CommandLine cmd = (new CommandLine.Builder(args))
                 .requireOptions("input-path", "matched-trips", "output-path")
-                .allowOptions("scenario-type", "csv-separator")
+                .allowOptions("scenario-type", "csv-separator", "update-passengers")
                 .build();
 
         String population = cmd.getOptionStrict("input-path");
@@ -37,6 +34,7 @@ public class AssignMultiplePassengersToCarDrivers {
         String outputfile = cmd.getOptionStrict("output-path");
         String scenarioType = cmd.hasOption("scenario-type") ? cmd.getOption("scenario-type").get() : "Group";
         String csvSeparator = cmd.hasOption("csv-separator") ? cmd.getOption("csv-separator").get() : ",";
+        Boolean isUpdatePassenger = cmd.hasOption("update-passengers") ? Boolean.parseBoolean(cmd.getOption("update-passengers").get()) : false;
 
         //Read in the passenger-driver matching
         BufferedReader reader = IOUtils.getBufferedReader(matchedTripsPath);
@@ -66,6 +64,8 @@ public class AssignMultiplePassengersToCarDrivers {
 
         Population populationData = scenario.getPopulation();
 
+        PopulationFactory popFactory = populationData.getFactory();
+
         List<Id<Person> > passengerList = new ArrayList<>();
         Map<Id<Person>, Person> matchedPassengers = new HashMap<>();
 
@@ -79,50 +79,43 @@ public class AssignMultiplePassengersToCarDrivers {
                 for (Leg leg : trip.getLegsOnly()) {
                     if (leg.getMode().equals("car") || leg.getMode().equals("car_passenger")) {
 
-                        //Assign passengers for group scenario
-                        if (scenarioType.equals("Group") && leg.getMode().equals("car")) {
-                            if (driversPassengers.get(driverId) != null) {
+                        //Assign passengers for group scenario and generate update pax data in norm scenario
+                        if (leg.getMode().equals("car") && driversPassengers.get(driverId) != null) {
                                 String reservedPassengers = "";
-                                //loop through the driver and passengers and convert to string
+                                //loop through the driver and passengers and convert to Ids
                                 int ind = 1;
                                 for (String id: driversPassengers.get(driverId)){
                                     if (ind > 1) {
                                         reservedPassengers = reservedPassengers + "," + id;
-                                        passengerList.add(Id.create(id, Person.class));
+                                        Id<Person> passengerId = Id.create(id, Person.class);
+                                        passengerList.add(passengerId);
+
+                                        if (isUpdatePassenger && scenarioType.equals("Norm")){
+                                            // create a new person with drivers' information for passengers
+                                            Person newPassenger = popFactory.createPerson(passengerId);
+
+                                            for (Plan plan: person.getPlans()){
+                                                newPassenger.addPlan(plan);
+                                            }
+                                            newPassenger.getAttributes().putAttribute("matchedDriver", person.getId().toString());
+                                            matchedPassengers.put(passengerId,newPassenger);
+                                        }
 
                                     } else {
                                         reservedPassengers = reservedPassengers + id;
                                     }
                                     ind++;
                                 }
-                                leg.getAttributes().putAttribute("reservedPassengers", reservedPassengers);
+                                if (scenarioType.equals("Group")){
+                                    leg.getAttributes().putAttribute("reservedPassengers", reservedPassengers);
 
-                                //also add this to person attribute since matsim sometimes deletes the leg attributes
-                                person.getAttributes().putAttribute("reservedPassengers", reservedPassengers);
-
-
-                            }
-                        }
-                        if (scenarioType.equals("Norm") && (driversPassengers.get(driverId) != null)){
-                            int ind = 1;
-                            for (String id: driversPassengers.get(driverId)){
-                                if (ind > 1) {
-                                    passengerList.add(Id.create(id, Person.class));
-
-                                    //ToDo create a person with drivers' information
-                                    Person newPassenger = null;
-                                    person.getPlans();
-
-                                    //matchedPassengers.put(Id.create(id, Person.class),newPassenger);
-
-                                    //ToDo give the person its an attribute of its initial personID?
-
+                                    //also add this to person attribute since matsim sometimes deletes the leg attributes
+                                    person.getAttributes().putAttribute("reservedPassengers", reservedPassengers);
                                 }
-                                ind++;
-                            }
+
                         }
 
-                                leg.setMode("drt");
+                        leg.setMode("drt");
                         leg.getAttributes().removeAttribute("routingMode");
                         leg.getAttributes().putAttribute("routingMode", "drt");
 
@@ -142,12 +135,24 @@ public class AssignMultiplePassengersToCarDrivers {
             System.out.println("Pop size with car passengers removed for group scenario: " + passengerList.size());
         }
 
-        //toDo update the matched passengers in Norm scenario to have same origin, destination and departure time as it's driver
-        if (scenarioType.equals("Norm")){
-            //remove passenger with old plan and add passeg again with their corresponding driver information
+        // update the matched passengers in Norm scenario to have same origin, destination and departure time as it's driver
+        if (isUpdatePassenger && scenarioType.equals("Norm")){
+            for (Id<Person> id: passengerList){
+                Person updatedPassenger = matchedPassengers.get(id);
+
+                Person oldPassenger = populationData.getPersons().get(id);
+                for (Map.Entry attr: oldPassenger.getAttributes().getAsMap().entrySet()){
+
+                    updatedPassenger.getAttributes().putAttribute(attr.getKey().toString(),attr.getValue());
+                }
+
+                //remove passenger with old plan and add again with their corresponding driver information
+                populationData.removePerson(id);
+
+                populationData.addPerson(updatedPassenger);
+            }
+
         }
-        //store driver's plan along with passenger list
-        //loop through all the passengers and update their plan
 
         System.out.println("Final Pop size: " +populationData.getPersons().size());
 
